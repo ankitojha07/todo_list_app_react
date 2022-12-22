f2f9ecf3e3a142947aa8351765cef56d6854c314
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-22 17:19:24 +0530

    nav added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+export const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={""} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toogle-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#projects">Contact</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icons">
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+              <a href="#">
+                <img src={""} alt="" />
+              </a>
+            </div>
+            <button
+              className="vvd"
+              onClick={() => {
+                console.log("Connect");
+              }}
+            >
+              {" "}
+              <span>Let's Connect</span>{" "}
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
